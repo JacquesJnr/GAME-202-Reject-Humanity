34aee3a8de833c374713ef4985b8326a5b35f1ee
Jacques Visser <jacquesvisserjnr@gmail.com>
2021-02-18 11:55:17 -0800

    Added Sprite Animations, Music & Custom Play Button

---
+++ Assets/Scripts/MonkeyAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonkeyAnimations : MonoBehaviour
+{
+    public RectTransform monke1, monke2;
+    public AnimationCurve curve;
+    public float duration;
+    public float delay;
+    public LeanTweenType easeType;
+
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        if(easeType == LeanTweenType.animationCurve)
+        {
+            LeanTween.rotate(monke1, 15.0f, duration).setDelay(delay).setLoopPingPong().setEase(curve);
+            LeanTween.rotate(monke2, 15.0f, duration).setDelay(delay).setLoopPingPong().setEase(curve);
+        }
+    }
+}
